ea8efc1e3206a62d549fdf71720ad8e39bb6f5cf
Christopher Carman <christopher.carman@podimo.com>
2021-12-08 11:10:19 +0100

    Day1

---
+++ 2021/day1/day1.lisp
@@ -0,0 +1,10 @@
+
+(defvar li (map 'list #'parse-integer (uiop:read-file-lines "day1_input.txt")) "list")
+
+(defun part1 (numbers)
+  (loop for (a b) on numbers
+    when b count (< a b)))
+
+(defun part2 (numbers)
+  (loop for (a b c) on numbers
+        when c collect (+ a b c)))

6c49896c8f40c8ea748cb075c1546337be4b30bf
Shadesfear <chris@thecarmans.com>
2022-12-05 00:03:06 +0100

    Day one, much better

--- 2022/rust-aoc/src/day1.rs
+++ 2022/rust-aoc/src/day1.rs
@@ -1,44 +1,31 @@
 use crate::solution;
 
 fn part1(input: &str) -> String {
-    let calories: Vec<Vec<i32>> = input.lines().fold(Vec::new(), |mut acc, line| {
-        let n = line;
-        if n == "" || acc.is_empty() {
-            acc.push(Vec::new());
-        } else {
-            acc.last_mut().unwrap().push(n.parse().unwrap())
-        }
-        acc
-    });
-    calories
-        .into_iter()
-        .map(|l| l.into_iter().sum::<i32>())
+    input
+        .split("\n\n")
+        .map(|calories| {
+            calories
+                .lines()
+                .filter_map(|s| s.parse::<u32>().ok())
+                .sum::<u32>()
+        })
         .max()
         .unwrap()
         .to_string()
 }
 
 fn part2(input: &str) -> String {
-    let calories: Vec<Vec<i32>> = input.lines().fold(Vec::new(), |mut acc, line| {
-        let n = line;
-        if n == "" || acc.is_empty() {
-            acc.push(Vec::new());
-        } else {
-            acc.last_mut().unwrap().push(n.parse().unwrap())
-        }
-        acc
-    });
-    let mut max_calories = calories
-        .into_iter()
-        .map(|l| l.into_iter().sum::<i32>())
-        .collect::<Vec<i32>>();
-
-    max_calories.sort();
-
-    max_calories[max_calories.len() - 3..]
-        .into_iter()
-        .sum::<i32>()
-        .to_string()
+    let mut calories: Vec<u32> = input
+        .split("\n\n")
+        .map(|calories| {
+            calories
+                .lines()
+                .filter_map(|s| s.parse::<u32>().ok())
+                .sum::<u32>()
+        })
+        .collect();
+    calories.sort();
+    calories.into_iter().rev().take(3).sum::<u32>().to_string()
 }
 
 pub fn solution() -> solution::Solution {

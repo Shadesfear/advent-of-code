fb494393516875370c9c0686f48466d9547c83e8
Christopher Carman <christopher.carman@podimo.com>
2024-12-10 10:05:22 +0100

    feat(tool): able to init go aoc day

---
+++ go-day-init/go.mod
@@ -0,0 +1,3 @@
+module gdi
+
+go 1.23.4
---
+++ go-day-init/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	_ "embed"
+	"fmt"
+	"os"
+)
+
+//go:embed main_go
+var MAIN string
+
+//go:embed main_test
+var TEST string
+
+func main() {
+	err := os.WriteFile("main.go", []byte(MAIN), 0644)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	err = os.WriteFile("main_test.go", []byte(TEST), 0644)
+	if err != nil {
+		fmt.Println(err)
+	}
+}

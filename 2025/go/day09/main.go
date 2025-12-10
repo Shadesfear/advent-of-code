f8031df9dae0429277b38ea50e0df6c8f081c365
Shadesfear <chris@thecarmans.com>
2025-12-10 17:16:22 +0100

    2025 day 09, raytracing boundary check

--- 2025/go/day09/main.go
+++ 2025/go/day09/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"log"
-	"math"
 	"strings"
 
 	"github.com/shadesfear/aoc-lib-go/files"
@@ -65,98 +64,116 @@ func Corners(a, b Point) []Point {
 	}
 }
 
-func inside(boundary []Point, p Point, minx, maxx, miny, maxy int) bool {
-	boundarySet := datastructures.NewSet[Point]()
-	boundarySet.AddM(boundary)
-
-	if boundarySet.Contains(p) {
-		return true
-	}
-
+func insidePolygon(vertices []Point, point Point) bool {
 	crossings := 0
-	cur := p
-	for cur.InBoundsA(minx, maxx, miny, maxy) {
-		if boundarySet.Contains(cur) {
-			crossings++
+	n := len(vertices)
+	for _, v := range vertices {
+		if v == point {
+			return true
 		}
-		cur = cur.Move(1, 1)
 	}
-	if crossings%2 == 0 {
-		// Not inside
-		return false
+	for i := range n {
+		a := vertices[i]
+		b := vertices[(i+1)%n]
+
+		if a.Y == b.Y {
+			continue
+		}
+
+		if a.Y > b.Y {
+			a, b = b, a
+		}
+
+		if point.Y >= a.Y && point.Y < b.Y {
+			if a.X > point.X {
+				crossings++
+			}
+		}
 	}
-	return true
+
+	return crossings%2 == 1
+
 }
 
-func insidePolygon(args) return type{
+func segmentsIntersects(h1, h2, v1, v2 Point) bool {
+	hY := h1.Y
+	vX := v1.X
 
+	hxMin, hxMax := min(h1.X, h2.X), max(h1.X, h2.X)
+	vyMin, vyMax := min(v1.Y, v2.Y), max(v1.Y, v2.Y)
+
+	return vX > hxMin && vX < hxMax && hY > vyMin && hY < vyMax
+}
+
+func rectangleCrossesPolygon(a, b Point, vertices []Point) bool {
+	minX, maxX := min(a.X, b.X), max(a.X, b.X)
+	minY, maxY := min(a.Y, b.Y), max(a.Y, b.Y)
+
+	top := [2]Point{{X: minX, Y: minY}, {X: maxX, Y: minY}}
+	bot := [2]Point{{X: minX, Y: maxY}, {X: maxX, Y: maxY}}
+	left := [2]Point{{X: minX, Y: minY}, {X: minX, Y: maxY}}
+	right := [2]Point{{X: maxX, Y: minY}, {X: maxX, Y: maxY}}
+
+	n := len(vertices)
+
+	for i := range n {
+		p1 := vertices[i]
+		p2 := vertices[(i+1)%n]
+
+		if p1.Y == p2.Y {
+			if segmentsIntersects(p1, p2, left[0], left[1]) {
+				return true
+			}
+			if segmentsIntersects(p1, p2, right[0], right[1]) {
+				return true
+			}
+		} else {
+			if segmentsIntersects(top[0], top[1], p1, p2) {
+				return true
+			}
+			if segmentsIntersects(bot[0], bot[1], p1, p2) {
+				return true
+			}
+		}
+	}
+	return false
 }
 
 func part2(points []Point) int {
 	var biggest int
+	var aa Point
+	var bb Point
 	n := len(points)
-	// First find green or red points on boundary
-	boundary := []Point{}
 
-	for i := 0; i < n; i++ {
-
-		next := points[(i+1)%n]
-		if next.X == points[i].X {
-			for j := min(points[i].Y, next.Y); j < max(points[i].Y, next.Y); j++ {
-				boundary = append(boundary, Point{X: points[i].X, Y: j})
-			}
-		} else {
-			for j := min(points[i].X, next.X); j < max(points[i].X, next.X); j++ {
-				boundary = append(boundary, Point{X: j, Y: points[i].Y})
-			}
-		}
-	}
-
-	var (
-		minx = math.MaxInt
-		maxx = 0
-		miny = math.MaxInt
-		maxy = 0
-	)
-
-	for _, point := range boundary {
-		if point.X < minx {
-			minx = point.X - 1
-		}
-		if point.X > maxx {
-			maxx = point.X + 1
-		}
-		if point.Y < miny {
-			miny = point.Y - 1
-		}
-		if point.Y > maxy {
-			maxy = point.Y + 1
-		}
-	}
-
-	for i := 0; i < n; i++ {
+	for i := range n {
 		for j := i + 1; j < n; j++ {
 			a, b := points[i], points[j]
 			corners := Corners(a, b)
 			insideb := true
 			for _, corner := range corners {
-				if !inside(boundary, corner, minx, maxx, miny, maxy) {
+				if !insidePolygon(points, corner) {
 					insideb = false
 					break
 				}
 			}
 
 			if insideb {
+				if rectangleCrossesPolygon(a, b, points) {
+					continue
+				}
 				dx := mymath.Abs(b.X - a.X)
 				dy := mymath.Abs(b.Y - a.Y)
 				area := (dx + 1) * (dy + 1)
 				if area >= biggest {
 					biggest = area
+					aa = a
+					bb = b
 				}
 			}
 
 		}
 	}
+	log.Println(aa, bb)
 	return biggest
 }
 

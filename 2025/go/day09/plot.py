34f01547db8ea71e5159f388075760d6ff9a2b4d
Shadesfear <chris@thecarmans.com>
2025-12-10 17:16:38 +0100

    plot of 2025 09

--- 2025/go/day09/plot.py
+++ 2025/go/day09/plot.py
@@ -9,6 +9,13 @@ import matplotlib.pyplot as plt
 def main() -> None:
     df = pd.read_csv("../../inputs/day09.txt", header=None)
     plt.plot(df[0], df[1])
+
+    p1 = (5953, 67629)
+    p2 = (94872, 50262)
+    rect_x = [p1[0], p2[0], p2[0], p1[0], p1[0]]
+    rect_y = [p1[1], p1[1], p2[1], p2[1], p1[1]]
+    plt.plot(rect_x, rect_y, 'r-', linewidth=2)
+
     plt.savefig("connected.png")
 
     print(df.head())

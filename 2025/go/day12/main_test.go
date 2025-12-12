57647c4302b11d4a0e73721e64678272927c9bc9
Shadesfear <chris@thecarmans.com>
2025-12-12 17:37:06 +0100

    2025 day 12 init

---
+++ 2025/go/day12/main_test.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestPart1(t *testing.T) {
+	input := []string{
+		"0:",
+		"###",
+		"##.",
+		"##.",
+		"",
+		"1:",
+		"###",
+		"##.",
+		".##",
+		"",
+		"2:",
+		".##",
+		"###",
+		"##.",
+		"",
+		"3:",
+		"##.",
+		"###",
+		"##.",
+		"",
+		"4:",
+		"###",
+		"#..",
+		"###",
+		"",
+		"5:",
+		"###",
+		".#.",
+		"###",
+		"",
+		"4x4: 0 0 0 0 2 0",
+		"12x5: 1 0 1 0 2 2",
+		"12x5: 1 0 1 0 3 2",
+	}
+	exp := 0
+	res := solvePart1(input)
+	if res != exp {
+		t.Errorf("solvePart1() = %d, exp %d", res, exp)
+	}
+}
+
+func TestPart2(t *testing.T) {
+	input := []string{
+		"",
+	}
+	exp := 0
+	res := solvePart2(input)
+	if res != exp {
+		t.Errorf("solvePart2() = %d, exp %d", res, exp)
+	}
+}
